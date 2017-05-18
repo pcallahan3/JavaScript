6269ef97fbf94cec2001c47d1b41da689a88e840
Patrick Callahan <pcallahan3@mail.greenriver.edu>
2017-05-18 00:14:03 -0500

    Added fizzbuzz.js file

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@
  *Write the FizzBuzz program described on page 38 of your text.
  */
     
-//loop to iterate over 
+//loop to iterate over 100
 for (var i=1; i <= 100; i++){
     if (i % 15 == 0)
         console.log("FizzBuzz");
